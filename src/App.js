cffaa70d32ae8c95436e1824a08f9321147fd09d
SimasDei <simondeiman@gmail.com>
2019-06-26 17:10:00 +0300

    add post api request and render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Post from './components/Post';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
   return (
     <div className="App">
       <h1>React Pagination</h1>
+      <ul className="list-group mb-4">
+        <Post />
+      </ul>
     </div>
   );
 }
